301c0618f43e967ebd3321a475931b8e9035ec63
A1rb0und3r1n0 <diekmann.johannes@web.de>
2025-06-06 14:47:57 +0200

    removed hard coded path and implemented sys.argv input

--- main.py
+++ main.py
@@ -6,21 +6,27 @@ from stats import sort_on
 import sys
 
 characters = {}
-filepath = "books/frankenstein.txt"
 
 def main():
-    count_characters(characters, filepath)
-    sorted_characters = split_dict(characters)
-    sorted_characters.sort(reverse=True, key=sort_on)
 
-    print("============ BOOKBOT ============")
-    print("Analyzing book found at " f"{filepath}" "...")
-    print("----------- Word Count ----------")
-    word_count = count_words(get_book_text(filepath))
-    print(f"Found {word_count} total words")
-    print("--------- Character Count -------")
-    for index, dict in enumerate(sorted_characters):
-        print(str(dict["char"]) + ": " + str(dict["num"]))
-    print("============= END ===============")
+    if len(sys.argv) > 1:
+        filepath = sys.argv[1]
+        count_characters(characters, filepath)
+        sorted_characters = split_dict(characters)
+        sorted_characters.sort(reverse=True, key=sort_on)
+
+        print("============ BOOKBOT ============")
+        print("Analyzing book found at " f"{filepath}" "...")
+        print("----------- Word Count ----------")
+        word_count = count_words(get_book_text(filepath))
+        print(f"Found {word_count} total words")
+        print("--------- Character Count -------")
+        for index, dict in enumerate(sorted_characters):
+            print(str(dict["char"]) + ": " + str(dict["num"]))
+        print("============= END ===============")
+    
+    else:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
 
 main()
